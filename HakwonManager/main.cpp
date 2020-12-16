8d5e5ba02f92644b48c72e7bdd3e73981f6b93a1
Yewon-dev <kyw20160785@gmail.com>
2020-12-16 23:57:00 +0900

    Initial Commit

---
+++ HakwonManager/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HakwonManager
+//
+//  Created by kimyewon on 2020/12/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
